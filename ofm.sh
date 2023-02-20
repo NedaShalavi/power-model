33c759d700c4f0ff23272b6bd477fe6a18a82048
NedaShalavi <91056884+NedaShalavi@users.noreply.github.com>
2023-02-20 12:49:14 +0000

    Add files via upload

---
+++ ofm.sh
@@ -0,0 +1,301 @@
+#!/bin/bash
+python3 test.py 5 1 32 28 3 1
+sleep 2
+python3 test.py 5 11 32 28 3 1
+sleep 2
+python3 test.py 5 21 32 28 3 1
+sleep 2
+python3 test.py 5 31 32 28 3 1
+sleep 2
+python3 test.py 5 41 32 28 3 1
+sleep 2
+python3 test.py 5 51 32 28 3 1
+sleep 2
+python3 test.py 5 61 32 28 3 1
+sleep 2
+python3 test.py 5 71 32 28 3 1
+sleep 2
+python3 test.py 5 81 32 28 3 1
+sleep 2
+python3 test.py 5 91 32 28 3 1
+sleep 2
+python3 test.py 5 101 32 28 3 1
+sleep 2
+python3 test.py 5 111 32 28 3 1
+sleep 2
+python3 test.py 5 121 32 28 3 1
+sleep 2
+python3 test.py 5 131 32 28 3 1
+sleep 2
+python3 test.py 5 141 32 28 3 1
+sleep 2
+python3 test.py 5 151 32 28 3 1
+sleep 2
+python3 test.py 5 161 32 28 3 1
+sleep 2
+python3 test.py 5 171 32 28 3 1
+sleep 2
+python3 test.py 5 181 32 28 3 1
+sleep 2
+python3 test.py 5 191 32 28 3 1
+sleep 2
+python3 test.py 5 201 32 28 3 1
+sleep 2
+python3 test.py 5 211 32 28 3 1
+sleep 2
+python3 test.py 5 221 32 28 3 1
+sleep 2
+python3 test.py 5 231 32 28 3 1
+sleep 2
+python3 test.py 5 241 32 28 3 1
+sleep 2
+python3 test.py 5 251 32 28 3 1
+sleep 2
+python3 test.py 5 261 32 28 3 1
+sleep 2
+python3 test.py 5 271 32 28 3 1
+sleep 2
+python3 test.py 5 281 32 28 3 1
+sleep 2
+python3 test.py 5 291 32 28 3 1
+sleep 2
+python3 test.py 5 301 32 28 3 1
+sleep 2
+python3 test.py 5 311 32 28 3 1
+sleep 2
+python3 test.py 5 321 32 28 3 1
+sleep 2
+python3 test.py 5 331 32 28 3 1
+sleep 2
+python3 test.py 5 341 32 28 3 1
+sleep 2
+python3 test.py 5 351 32 28 3 1
+sleep 2
+python3 test.py 5 361 32 28 3 1
+sleep 2
+python3 test.py 5 371 32 28 3 1
+sleep 2
+python3 test.py 5 381 32 28 3 1
+sleep 2
+python3 test.py 5 391 32 28 3 1
+sleep 2
+python3 test.py 5 401 32 28 3 1
+sleep 2
+python3 test.py 5 411 32 28 3 1
+sleep 2
+python3 test.py 5 421 32 28 3 1
+sleep 2
+python3 test.py 5 431 32 28 3 1
+sleep 2
+python3 test.py 5 441 32 28 3 1
+sleep 2
+python3 test.py 5 451 32 28 3 1
+sleep 2
+python3 test.py 5 461 32 28 3 1
+sleep 2
+python3 test.py 5 471 32 28 3 1
+sleep 2
+python3 test.py 5 481 32 28 3 1
+sleep 2
+python3 test.py 5 491 32 28 3 1
+sleep 2
+python3 test.py 5 501 32 28 3 1
+sleep 2
+python3 test.py 5 511 32 28 3 1
+sleep 2
+python3 test.py 5 521 32 28 3 1
+sleep 2
+python3 test.py 5 531 32 28 3 1
+sleep 2
+python3 test.py 5 541 32 28 3 1
+sleep 2
+python3 test.py 5 551 32 28 3 1
+sleep 2
+python3 test.py 5 561 32 28 3 1
+sleep 2
+python3 test.py 5 571 32 28 3 1
+sleep 2
+python3 test.py 5 581 32 28 3 1
+sleep 2
+python3 test.py 5 591 32 28 3 1
+sleep 2
+python3 test.py 5 601 32 28 3 1
+sleep 2
+python3 test.py 5 611 32 28 3 1
+sleep 2
+python3 test.py 5 621 32 28 3 1
+sleep 2
+python3 test.py 5 631 32 28 3 1
+sleep 2
+python3 test.py 5 641 32 28 3 1
+sleep 2
+python3 test.py 5 651 32 28 3 1
+sleep 2
+python3 test.py 5 661 32 28 3 1
+sleep 2
+python3 test.py 5 671 32 28 3 1
+sleep 2
+python3 test.py 5 681 32 28 3 1
+sleep 2
+python3 test.py 5 691 32 28 3 1
+sleep 2
+python3 test.py 5 701 32 28 3 1
+sleep 2
+python3 test.py 5 711 32 28 3 1
+sleep 2
+python3 test.py 5 721 32 28 3 1
+sleep 2
+python3 test.py 5 731 32 28 3 1
+sleep 2
+python3 test.py 5 741 32 28 3 1
+sleep 2
+python3 test.py 5 751 32 28 3 1
+sleep 2
+python3 test.py 5 761 32 28 3 1
+sleep 2
+python3 test.py 5 771 32 28 3 1
+sleep 2
+python3 test.py 5 781 32 28 3 1
+sleep 2
+python3 test.py 5 791 32 28 3 1
+sleep 2
+python3 test.py 5 801 32 28 3 1
+sleep 2
+python3 test.py 5 811 32 28 3 1
+sleep 2
+python3 test.py 5 821 32 28 3 1
+sleep 2
+python3 test.py 5 831 32 28 3 1
+sleep 2
+python3 test.py 5 841 32 28 3 1
+sleep 2
+python3 test.py 5 851 32 28 3 1
+sleep 2
+python3 test.py 5 861 32 28 3 1
+sleep 2
+python3 test.py 5 871 32 28 3 1
+sleep 2
+python3 test.py 5 881 32 28 3 1
+sleep 2
+python3 test.py 5 891 32 28 3 1
+sleep 2
+python3 test.py 5 901 32 28 3 1
+sleep 2
+python3 test.py 5 911 32 28 3 1
+sleep 2
+python3 test.py 5 921 32 28 3 1
+sleep 2
+python3 test.py 5 931 32 28 3 1
+sleep 2
+python3 test.py 5 941 32 28 3 1
+sleep 2
+python3 test.py 5 951 32 28 3 1
+sleep 2
+python3 test.py 5 961 32 28 3 1
+sleep 2
+python3 test.py 5 971 32 28 3 1
+sleep 2
+python3 test.py 5 981 32 28 3 1
+sleep 2
+python3 test.py 5 991 32 28 3 1
+sleep 2
+python3 test.py 5 1001 32 28 3 1
+sleep 2
+python3 test.py 5 1011 32 28 3 1
+sleep 2
+python3 test.py 5 1021 32 28 3 1
+sleep 2
+python3 test.py 5 1031 32 28 3 1
+sleep 2
+python3 test.py 5 1041 32 28 3 1
+sleep 2
+python3 test.py 5 1051 32 28 3 1
+sleep 2
+python3 test.py 5 1061 32 28 3 1
+sleep 2
+python3 test.py 5 1071 32 28 3 1
+sleep 2
+python3 test.py 5 1081 32 28 3 1
+sleep 2
+python3 test.py 5 1091 32 28 3 1
+sleep 2
+python3 test.py 5 1101 32 28 3 1
+sleep 2
+python3 test.py 5 1111 32 28 3 1
+sleep 2
+python3 test.py 5 1121 32 28 3 1
+sleep 2
+python3 test.py 5 1131 32 28 3 1
+sleep 2
+python3 test.py 5 1141 32 28 3 1
+sleep 2
+python3 test.py 5 1151 32 28 3 1
+sleep 2
+python3 test.py 5 1161 32 28 3 1
+sleep 2
+python3 test.py 5 1171 32 28 3 1
+sleep 2
+python3 test.py 5 1181 32 28 3 1
+sleep 2
+python3 test.py 5 1191 32 28 3 1
+sleep 2
+python3 test.py 5 1201 32 28 3 1
+sleep 2
+python3 test.py 5 1211 32 28 3 1
+sleep 2
+python3 test.py 5 1221 32 28 3 1
+sleep 2
+python3 test.py 5 1231 32 28 3 1
+sleep 2
+python3 test.py 5 1241 32 28 3 1
+sleep 2
+python3 test.py 5 1251 32 28 3 1
+sleep 2
+python3 test.py 5 1261 32 28 3 1
+sleep 2
+python3 test.py 5 1271 32 28 3 1
+sleep 2
+python3 test.py 5 1281 32 28 3 1
+sleep 2
+python3 test.py 5 1291 32 28 3 1
+sleep 2
+python3 test.py 5 1301 32 28 3 1
+sleep 2
+python3 test.py 5 1311 32 28 3 1
+sleep 2
+python3 test.py 5 1321 32 28 3 1
+sleep 2
+python3 test.py 5 1331 32 28 3 1
+sleep 2
+python3 test.py 5 1341 32 28 3 1
+sleep 2
+python3 test.py 5 1351 32 28 3 1
+sleep 2
+python3 test.py 5 1361 32 28 3 1
+sleep 2
+python3 test.py 5 1371 32 28 3 1
+sleep 2
+python3 test.py 5 1381 32 28 3 1
+sleep 2
+python3 test.py 5 1391 32 28 3 1
+sleep 2
+python3 test.py 5 1401 32 28 3 1
+sleep 2
+python3 test.py 5 1411 32 28 3 1
+sleep 2
+python3 test.py 5 1421 32 28 3 1
+sleep 2
+python3 test.py 5 1431 32 28 3 1
+sleep 2
+python3 test.py 5 1441 32 28 3 1
+sleep 2
+python3 test.py 5 1451 32 28 3 1
+sleep 2
+python3 test.py 5 1461 32 28 3 1
+sleep 2
+python3 test.py 5 1471 32 28 3 1
+sleep 2
+python3 test.py 5 1481 32 28 3 1
+sleep 2
+python3 test.py 5 1491 32 28 3 1
+sleep 2
