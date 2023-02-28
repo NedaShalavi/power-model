ba92598f89e329378c3afc324b460a6a06e2a6db
NedaShalavi <91056884+NedaShalavi@users.noreply.github.com>
2023-02-28 16:03:55 +0000

    Update test.py

--- dense/test.py
+++ dense/test.py
@@ -7,8 +7,6 @@ import numpy as np
 import csv
 import sys
 import torch
-import torchvision
-import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
